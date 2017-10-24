ac98aaee5cd78e53ff63af4610b2488597a91e59
casperlamboo <casperlamboo@gmail.com>
2017-10-24 15:51:40 +0200

    caps

--- src/shape/index.js
+++ src/shape/index.js
@@ -1,6 +1,6 @@
 import shapeToPoints from './shapeToPoints.js';
 import JSONToSketchData from './JSONToSketchData.js';
-import ShapeDataUtils from './ShapeDataUtils.js';
+import ShapeDataUtils from './shapeDataUtils.js';
 import shapeTypeProperties from './shapeTypeProperties.js';
 
 export { shapeToPoints, JSONToSketchData, shapeTypeProperties };
