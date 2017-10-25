ef761911415e155d2196df96e51e30ac0acc9a72
casperlamboo <casperlamboo@gmail.com>
2017-10-25 11:54:42 +0200

    remove math

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import * as math from './math/index.js';
 import * as shape from './shape/index.js';
 import * as utils from './utils/index.js';
 import * as d3 from './d3/index.js';
 import * as components from './components/index.js';
 import * as constants from './constants/index.js';
 
-export { math, shape, utils, d3, components, constants };
+export { shape, utils, d3, components, constants };
