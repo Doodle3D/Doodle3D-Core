c062af4bd9aa734e78ae0c1091122a6afd369831
casperlamboo <casperlamboo@gmail.com>
2017-11-02 17:24:39 +0100

    rename func

--- src/shape/index.js
+++ src/shape/index.js
@@ -1,6 +1,6 @@
-import dbToDoc from './dbToDoc.js';
+import docToFile from './docToFile.js';
 import * as shapeToPoints from './shapeToPoints.js';
 import JSONToSketchData from './JSONToSketchData.js';
 import * as shapeDataUtils from './shapeDataUtils.js';
 
-export { dbToDoc, shapeToPoints, JSONToSketchData, shapeDataUtils };
+export { docToFile, shapeToPoints, JSONToSketchData, shapeDataUtils };
