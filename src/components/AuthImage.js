193ed418b950d84b650e4ac44288fb255cff9a03
casperlamboo <casperlamboo@gmail.com>
2017-11-02 17:38:04 +0100

    import proptypes serperatly

--- src/components/AuthImage.js
+++ src/components/AuthImage.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'proptypes';
 import { connect } from 'react-redux';
 import { isWebUri } from 'valid-url';
 import PouchDB from 'pouchdb';
