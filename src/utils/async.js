a9038e2e303e95b9a77490e1ff4669951ea60b22
casperlamboo <casperlamboo@gmail.com>
2017-11-08 23:39:58 +0100

    add export statement

--- src/utils/async.js
+++ src/utils/async.js
@@ -43,7 +43,7 @@ export function asyncIterator(array, callback) {
 //   return result;
 // }
 
-function createThrottle() {
+export function createThrottle() {
   let next = null;
 
   return callback => {
