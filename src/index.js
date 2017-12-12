aa9e15fb45c6f88592ccdf0bddba972b3a3ddf44
casperlamboo <casperlamboo@gmail.com>
2017-12-12 11:31:11 +0100

    removed indexes

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import * as shape from './shape/index.js';
-import * as utils from './utils/index.js';
-import * as d3 from './d3/index.js';
-import * as components from './components/index.js';
-import * as constants from './constants/index.js';
-import * as actions from './actions/index.js';
-import * as reducer from './reducer/index.js';
-
-export { shape, utils, d3, components, constants, actions, reducer };
