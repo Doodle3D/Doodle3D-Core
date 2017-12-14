6a9e649ee7c5ece4f786877a440522fc01653285
casperlamboo <casperlamboo@gmail.com>
2017-12-14 11:18:41 +0100

    css

--- index.js
+++ index.js
@@ -32,14 +32,14 @@ import jss from 'jss';
 import preset from 'jss-preset-default';
 import normalize from 'normalize-jss';
 jss.setup(preset());
+jss.createStyleSheet(normalize).attach();
 jss.createStyleSheet({
-  '@global body, html, #app': {
-    height: '100%'
-  },
-  '@global body': {
-    overflow: 'hidden'
-  },
-  ...normalize
+  '@global': {
+    '*': { margin: 0, padding: 0 },
+    '#app, body, html': { height: '100%', fontFamily: 'sans-serif' },
+    body: { overflow: 'auto' },
+    html: { overflow: 'hidden' }
+  }
 }).attach();
 
 // render dom
