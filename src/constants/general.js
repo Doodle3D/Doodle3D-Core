3670802beab3fa007ffb45608b16f0e849c6833e
Casper Lamboo <casperlamboo@gmail.com>
2018-06-25 12:10:29 +0200

    bumb version

--- src/constants/general.js
+++ src/constants/general.js
@@ -1,6 +1,6 @@
 import * as contextTools from './contextTools.js';
 
-export const VERSION = '0.20.0';
+export const VERSION = '0.21.0';
 export const SHAPE_CACHE_LIMIT = 50;
 export const PIXEL_RATIO = 1.0;
 
